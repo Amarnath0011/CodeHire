21e84cc9108553f76ee60b5b209e116eaf345271
Amarnath <amarnathkumar9305@gmail.com>
2026-08-14 20:33:37 +0530

    Add recruiter application service

--- Frontend/vite-project/src/services/applicationService.js
+++ Frontend/vite-project/src/services/applicationService.js
@@ -1,16 +1,14 @@
 import api from "./api";
 
-export const applyToJob = (data) =>
-  api.post("/applications", data);
+export const applyToJob = (data) => api.post("/applications", data);
 
-export const getApplicants = (jobId) =>
-  api.get(`/applications/${jobId}`);
+export const getApplicants = (jobId) => api.get(`/applications/${jobId}`);
 
-export const updateApplicationStatus =(id,data) =>
-    api.put(`/applications/${id}`, data);
+export const updateApplicationStatus = (id, data) =>
+  api.put(`/applications/${id}`, data);
 
-export const getMyApplications =
-  (studentId) =>
-    api.get(
-      `/applications/student/${studentId}`
-    );
+export const getMyApplications = (studentId) =>
+  api.get(`/applications/student/${studentId}`);
+
+export const getRecruiterApplications = () =>
+  api.get("/applications/recruiter");
