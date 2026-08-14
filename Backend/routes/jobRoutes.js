b8d5dd238285bbc965d1a33609370582dfc4ca59
Amarnath <amarnathkumar9305@gmail.com>
2026-08-14 20:35:18 +0530

    Expose recruiter jobs endpoint

--- Backend/routes/jobRoutes.js
+++ Backend/routes/jobRoutes.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  createJob,
-  getJobs,
-  deleteJob,
-} = require("../controllers/jobController");
+const { createJob, getJobs, getRecruiterJobs, deleteJob } = require("../controllers/jobController");
 const { protect } = require("../middleware/authMiddleware");
 
+router.get("/recruiter", protect, getRecruiterJobs);
 router.post("/", protect, createJob);
 router.get("/", getJobs);
 router.delete("/:id", protect, deleteJob);
