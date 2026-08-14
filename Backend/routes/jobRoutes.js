ac27d756f0d89bb3a90d63ec20a0c1638e30cd17
Amarnath <amarnathkumar9305@gmail.com>
2026-08-14 21:40:58 +0530

    Add recruiter jobs route before dynamic id route

--- Backend/routes/jobRoutes.js
+++ Backend/routes/jobRoutes.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
 const {
   createJob,
   getJobs,
+  getRecruiterJobs,
   deleteJob,
 } = require("../controllers/jobController");
 
 router.post("/", createJob);
 router.get("/", getJobs);
+router.get("/recruiter", protect, getRecruiterJobs);
 router.delete("/:id", deleteJob);
 
 module.exports = router;
